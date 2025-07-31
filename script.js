d4961c8f752ba831bdc808aac229e6d01d5bd2c8
indrianinh <indrianinurhidayah212@gmail.com>
2025-07-30 22:04:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //musik
 const musik = document.getElementById("bg-music");
-const btn = doccument.getElementById("music-btn");
+const btn = doccument
+  .getElementById("music-btn")
+  .addEventListener("Click", () => {
+    musik.play();
+  });
 let isPlaying = false;
