243324e876de4bf07c2a8c7c0a8788ae66dea460
indrianinh <indrianinurhidayah212@gmail.com>
2025-07-30 22:11:40 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-//musik
 const musik = document.getElementById("bg-music");
-const btn = doccument
-  .getElementById("music-btn")
-  .addEventListener("Click", () => {
-    musik.play();
-  });
+const btn = document.getElementById("music-btn");
 let isPlaying = false;
+
+btn.addEventListener("click", () => {
+  if (!isPlaying) {
+    musik
+      .play()
+      .then(() => {
+        isPlaying = true;
+        btn.textContent = "Pause Music"; // ubah tulisan tombol
+      })
+      .catch((err) => {
+        console.log("Play blocked:", err);
+      });
+  } else {
+    musik.pause();
+    isPlaying = false;
+    btn.textContent = "Play Music"; // ubah tulisan tombol
+  }
+});
